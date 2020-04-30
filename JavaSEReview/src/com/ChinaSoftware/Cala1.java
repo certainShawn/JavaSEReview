ac07e3f70b6fe7723b9df22095cc0f2cdf8d4bf1
A_certain_Shawn <alex.y0203@gmail.com>
2020-04-30 23:29:22 +0900

    Add files via upload

---
+++ JavaSEReview/src/com/ChinaSoftware/Cala1.java
@@ -0,0 +1,13 @@
+package com.ChinaSoftware;
+
+public class Cala1 {
+    public static void main(String args[])
+    {
+        int num=0;
+        for(int i=1;i<=100;i++)
+        {
+            num=i+num;
+            System.out.println(num);
+        }       
+    }
+}
