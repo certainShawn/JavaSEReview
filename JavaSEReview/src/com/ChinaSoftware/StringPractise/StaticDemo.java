7b1a7ea4b2535ed03e7c4a7a64ac246b9a6eaa34
A_certain_Shawn <alex.y0203@gmail.com>
2020-08-30 23:55:30 +0900

    Add files via upload

---
+++ JavaSEReview/src/com/ChinaSoftware/StringPractise/StaticDemo.java
@@ -0,0 +1,18 @@
+package com.ChinaSoftware.StringPractise;
+/**
+ * 静态变量:类名称.静态变量
+ * 静态方法:类名称.静态方法()
+ * 
+ * 注意事项:
+ * 1:静态不能直接访问非静态。
+ * 原因:因为在内存当中是先有静态的内容同，后有非静态的内容。
+ */
+public class StaticDemo {
+        public static void main(String args[])
+        {
+            /**
+             * 对于静态方法来说，可以通过对象名进行调用，也可以直接通过类名称来调用。
+             */
+        }
+    
+}
